3fd24833d1b9e7289b57fd22c6cb089d1e67178d
Dave Humphrey <dave@uesp.net>
2016-04-04 16:49:11 -0400

    fix for showing skill browser

--- UespEsoSkills.php
+++ UespEsoSkills.php
@@ -9,14 +9,47 @@
  *
  */
 
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo <<<EOT
+To install my extension, put the following line in LocalSettings.php:
+require_once( "\$IP/extensions/EsoCharData/EsoCharData.php" );
+EOT;
+	exit( 1 );
+}
+
+
+require_once("/home/uesp/secrets/esolog.secrets");
+require_once('/home/uesp/esolog.static/viewSkills.class.php');
+
+
+$wgExtensionCredits['specialpage'][] = array(
+		'path' => __FILE__,
+		'name' => 'EsoSkills',
+		'author' => 'Dave Humphrey (dave@uesp.net)',
+		'url' => 'http://www.uesp.net/wiki/UESPWiki:EsoSkills',
+		'descriptionmsg' => 'esoskills-desc',
+		'version' => '0.1.0',
+);
+
+
+$wgAutoloadClasses['SpecialEsoSkills'] = __DIR__ . '/SpecialEsoSkills.php';
+$wgMessagesDirs['EsoSkills'] = __DIR__ . "/i18n";
+$wgExtensionMessagesFiles['EsoSkills'] = __DIR__ . '/EsoSkills.alias.php';
+$wgSpecialPages['EsoSkills'] = 'SpecialEsoSkills';
+
 $wgHooks['ParserFirstCallInit'][] = 'UespEsoSkillsParserInit';
 $wgHooks['BeforePageDisplay'][] = 'uesoEsoSkills_beforePageDisplay';
+
 
 function uesoEsoSkills_beforePageDisplay(&$out) {
 	global $wgScriptPath;
 	
 	$out->addHeadItem("uesp-esoskills-css", "<link rel='stylesheet' href='$wgScriptPath/extensions/UespEsoSkills/uespesoskills.css?4Apr2016' />");
 	$out->addHeadItem("uesp-esoskills-js", "<script src='$wgScriptPath/extensions/UespEsoSkills/uespesoskills.js?4Apr2016'></script>");
+	
+	$out->addHeadItem("uesp-esoskillsbrowser-css", "<link rel='stylesheet' href='http://esolog.uesp.net/resources/esoskills_embed.css' />");
+	$out->addHeadItem("uesp-esoskillsbrowser-js", "<script src='http://esolog.uesp.net/resources/esoskills.js'></script>");
 	
 	return true;
 }
